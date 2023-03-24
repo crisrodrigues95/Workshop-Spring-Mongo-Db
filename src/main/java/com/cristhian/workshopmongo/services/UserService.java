f60f2caf88e4a680832bcb3c2a05ac96021ac3a6
Cristhian Rodrigues <cristhian.rodrigues95@gmail.com>
2023-03-24 15:28:27 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/cristhian/workshopmongo/services/UserService.java
@@ -0,0 +1,22 @@
+package com.cristhian.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cristhian.workshopmongo.domain.User;
+import com.cristhian.workshopmongo.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+		
+	}
+
+}
